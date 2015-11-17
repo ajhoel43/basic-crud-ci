30c1c03da7c4c7aef9722b0ebeedc9a12bdb750a
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2015-11-17 16:52:44 +0700

    edit libraries & helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'url_helper', 'form');
+$autoload['libraries'] = array('database', 'form_validation');
 
 /*
 | -------------------------------------------------------------------
@@ -83,7 +83,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'form_validation');
+$autoload['helper'] = array('url', 'form');
 
 /*
 | -------------------------------------------------------------------
